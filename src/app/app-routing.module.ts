d49f76f827a9102572037380409c655f6edb15e3
sandrine <sandrinezocca@gmail.com>
2020-03-23 14:02:07 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'heroes', component: HeroListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
