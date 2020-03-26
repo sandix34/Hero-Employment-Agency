d00d87bf0530ca7a362357a6c85dd631fbe4ce8b
sandrine <sandrinezocca@gmail.com>
2020-03-26 13:53:00 +0100

    Route Parameters

--- src/app/heroes/hero-detail/hero-detail.component.ts
+++ src/app/heroes/hero-detail/hero-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { HeroService }  from '../hero.service';
+import { Hero } from '../hero';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  // The prop$ dollar suffix is generally used to indicate something is an Observable source.
+  hero$: Observable<Hero>;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    // private router: Router,
+    private service: HeroService
+  ) {}
+
+  ngOnInit() {
+    this.hero$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.service.getHero(params.get('id')))
+    );
   }
 
 }
--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,12 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes
     return of(HEROES);
+  }
+
+  getHero(id: number | string) {
+    return this.getHeroes().pipe(
+      // (+) before `id` turns the string into a number
+      map((heroes: Hero[]) => heroes.find(hero => hero.id === +id))
+    );
   }
 }
